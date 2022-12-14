a841c24f78041e001e27124410dc3d95a9d1e463
AbdulTheActivePiecer <abdulyki@activepieces.com>
2022-12-14 16:43:35 +0300

    hide column action as it doesn't work like it should

--- src/components/src/apps/google-sheets/index.ts
+++ src/components/src/apps/google-sheets/index.ts
@@ -1,10 +1,9 @@
 import {createComponent} from '../../framework/component';
-import {insertColumnAction} from './actions/insert-column.action';
 import {insertRowAction} from './actions/insert-row.action';
 
 export const googleSheets = createComponent({
 	name: 'Google Sheets',
 	logoUrl: 'https://cdn.activepieces.com/components/googlesheets/logo.png',
-	actions: [insertColumnAction, insertRowAction],
+	actions: [insertRowAction],
 	triggers: [],
 });
